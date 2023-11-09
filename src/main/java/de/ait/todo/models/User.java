4a65b004938b7aa3d3a4dc3a6dfb958132f7fed9
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2023-11-09 19:54:39 +0100

    add 14

--- src/main/java/de/ait/todo/models/User.java
+++ src/main/java/de/ait/todo/models/User.java
@@ -1,11 +1,13 @@
 package de.ait.todo.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -23,19 +25,31 @@ import java.util.List;
 public class User {
 
     public enum Role {
-        USER, ADMIN
+        ADMIN, USER, DOGSITTER, DOGLOVER
+    }
+
+    public enum State {
+        NOT_CONFIRMED, CONFIRMED, DELETED, BANNED
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    private String userName;
+
+    @Column(nullable = false, unique = true)
     private String email;
     private String hashPassword;
 
     @Enumerated(value = EnumType.STRING)
     private Role role;
 
-    @OneToMany(mappedBy = "user")
-    private List<Task> tasks;
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private State state;
+
+//    @OneToMany(mappedBy = "user")
+//    private List<Task> tasks;
 }
