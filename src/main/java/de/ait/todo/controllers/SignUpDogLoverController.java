0de3ef6548d067ad4cabbe11198cd42947afb8df
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2023-11-10 21:19:24 +0100

    add 46

--- src/main/java/de/ait/todo/controllers/SignUpDogLoverController.java
+++ src/main/java/de/ait/todo/controllers/SignUpDogLoverController.java
@@ -6,6 +6,7 @@ import de.ait.todo.dto.DogLoverDto;
 import de.ait.todo.services.SignUpDogLoverService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin
 public class SignUpDogLoverController implements SignUpDogLover {
 
     private final SignUpDogLoverService signUpDogLoverService;
