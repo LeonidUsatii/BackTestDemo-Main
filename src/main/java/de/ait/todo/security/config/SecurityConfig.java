84ce7a72b863107d962a5f3b65cff56fc75bf653
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2023-11-11 21:25:11 +0100

    add 57

--- src/main/java/de/ait/todo/security/config/SecurityConfig.java
+++ src/main/java/de/ait/todo/security/config/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.POST, "/api/registerUser/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/registerSetter/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/dog-sitters/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api//users/my/profile/**").permitAll()
                 .antMatchers("/api/users/confirm/**").permitAll()
                 .antMatchers("/api/clinics/**").permitAll()
                 .antMatchers("/api/kennels/**").permitAll()
